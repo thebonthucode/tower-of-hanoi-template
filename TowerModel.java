e0c0337382cd33d8cfc12e82c852915e82656150
Shasank Bonthu <154383363+thebonthucode@users.noreply.github.com>
2025-03-30 02:07:35 +0000

    final

--- TowerModel.java
+++ TowerModel.java
@@ -1,69 +1,52 @@
 public class TowerModel {
 
-    // 2d array storing game state
     private IntegerStack[] towers;
+    private int towerHeight;
+    private int printCounter;
+    private int moveCounter;
 
-    // Final tower height
-    private int towerHeight = 0;
-
-    // Debug metrics
-    private int printCounter = 0;
-    private int moveCounter = 0;
-
-    /* This class implements a model of a tower of Hanoi game.
-
-        |    |    |
-        =    |    |
-       ===   |    |
-      =====  |    |
-     ------------------
-        0    1    2
-
-     Example of a game of height three in the starting position.
-
-    */
-    public TowerModel(int height)
-    {
+    public TowerModel(int height) {
         towerHeight = height;
         towers = new IntegerStack[3];
         towers[0] = new IntegerStack(height);
         towers[1] = new IntegerStack(height);
         towers[2] = new IntegerStack(height);
 
-        for (int i=0; i<height; i++)
-        {
-            towers[0].push(height - i);
+        for (int i = height; i > 0; i--) {
+            towers[0].push(i);
         }
     }
 
-    // get the total number of disks
-    public int height()
-    {
+    public int height() {
         return towerHeight;
     }
 
+    public void move(int source, int destination) {
+        if (towers[source].peek() == 0 && towers[source].get(0) == 0) {
+            System.out.println("Invalid move attempted from " + source + " to " + destination);
+            return;
+        }
 
-    // Move one disk from the source stack to the destination stack.
-    public void move(int source, int destination)
-    {
-        System.out.println("Move #" + ++moveCounter + " from " + source + " to " + destination);
-        // TODO!!
+        if (towers[destination].peek() == 0 && towers[destination].get(0) == 0 || towers[source].peek() < towers[destination].peek()) {
+
+            int disk = towers[source].pop();
+            towers[destination].push(disk);
+            System.out.println("Move #" + ++moveCounter + " from " + source + " to " + destination);
+        } else {
+            System.out.println("Invalid move attempted from " + source + " to " + destination);
+        }
     }
 
-    // Helper method to nicely print the current model state.
-    public void print()
-    {
+    public void print() {
         System.out.print("Print #" + ++printCounter + " Towers of Hanoi\n");
 
-        for (int layer = towerHeight-1; layer >= 0; layer--){
+        for (int layer = towerHeight - 1; layer >= 0; layer--) {
             System.out.print("\n");
             for (int tower = 0; tower < towers.length; tower++) {
                 int value = towers[tower].get(layer);
-                if (value == 0)
-                {
+                if (value == 0) {
                     System.out.print(" |");
-                } else
-                {
+                } else {
                     System.out.print(" " + value);
                 }
             }
@@ -75,14 +58,11 @@ public class TowerModel {
         System.out.println();
     }
 
-    // Test instrumentation
-    public IntegerStack[] getTowers()
-    {
+    public IntegerStack[] getTowers() {
         return towers;
     }
 
-    public int getHeight()
-    {
+    public int getHeight() {
         return towerHeight;
     }
 }
--- TowerSolver.java
+++ TowerSolver.java
@@ -1,22 +1,20 @@
-
 public class TowerSolver {
     private TowerModel model;
 
-    public TowerSolver()
-    {
+    public TowerSolver() {
         // Nothing to do here
     }
 
-    public void solve(TowerModel model)
-    {
+    public void solve(TowerModel model) {
         this.model = model;
-        // Call the missing solve method (not this one)
+        solve(model.getHeight(), 0, 2, 1);
     }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
-
+    private void solve(int n, int source, int destination, int auxiliary) {
+        if (n <= 0) return;
+        
+        solve(n - 1, source, auxiliary, destination);
+        model.move(source, destination);
+        solve(n - 1, auxiliary, destination, source);
+    }
 }
